65c1975bba157b5bd1269a2bd9a4a8cbfe9105c5
Steve OC <stephen@ocallaghan.me.uk>
2016-09-09 22:28:44 +0100

    Final static todos

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get ('/', function(req, res) {
 	res.send('Todo API root');
 });
 
-// get /todos?completed=true
+// get /todos?completed=true&q=work
 app.get ('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -25,6 +25,12 @@ app.get ('/todos', function(req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if  (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ){
+		filteredTodos = _.filter(filteredTodos, function (todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodos);
