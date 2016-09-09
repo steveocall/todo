f2da28f9a17f0f9c560963e27b6dc279169c60f2
Steve OC <stephen@ocallaghan.me.uk>
2016-09-09 21:55:31 +0100

    Added ?completed filter

--- server.js
+++ server.js
@@ -16,9 +16,18 @@ app.get ('/', function(req, res) {
 	res.send('Todo API root');
 });
 
-// get / todos
+// get /todos?completed=true
 app.get ('/todos', function(req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if  (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 //get /todos/:id
